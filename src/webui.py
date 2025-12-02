62e8e9f6490d52ff7c9054ec7c9e3e3386dbbee1
DDME36 <satayu.pongpan@gmail.com>
2025-12-02 23:37:38 +0700

    Update webui.py

--- src/webui.py
+++ src/webui.py
@@ -211,7 +211,7 @@ if __name__ == '__main__':
                     rms_mix_rate = gr.Slider(0, 1, value=0.25, label='RMS mix rate', info="Control how much to mimic the original vocal's loudness (0) or a fixed loudness (1)")
                     protect = gr.Slider(0, 0.5, value=0.33, label='Protect rate', info='Protect voiceless consonants and breath sounds. Set to 0.5 to disable.')
                     with gr.Column():
-                        f0_method = gr.Dropdown(['rmvpe', 'fcpe', 'hybrid', 'mangio-crepe'], value='rmvpe', label='Pitch detection algorithm', info='rmvpe: clear vocals | fcpe: smoother, less robotic | hybrid: best quality (slower) | mangio-crepe: smoothest but slowest')
+                        f0_method = gr.Dropdown(['mangio-crepe', 'hybrid', 'fcpe', 'rmvpe'], value='mangio-crepe', label='Pitch detection algorithm', info='mangio-crepe: smoothest pitch (default) | hybrid[rmvpe+fcpe]: best quality (slower) | fcpe: smoother, less robotic | rmvpe: clear vocals')
                         crepe_hop_length = gr.Slider(32, 320, value=128, step=1, visible=False, label='Crepe hop length', info='Lower values leads to longer conversions and higher risk of voice cracks, but better pitch accuracy.')
                         f0_method.change(show_hop_slider, inputs=f0_method, outputs=crepe_hop_length)
                 keep_files = gr.Checkbox(label='Keep intermediate files', info='Keep all audio files generated in the song_output/id directory, e.g. Isolated Vocals/Instrumentals. Leave unchecked to save space')
@@ -246,7 +246,7 @@ if __name__ == '__main__':
                                        protect, pitch_all, reverb_rm_size, reverb_wet, reverb_dry, reverb_damping,
                                        output_format],
                                outputs=[ai_cover])
-            clear_btn.click(lambda: [0, 0, 0, 0, 0.5, 3, 0.25, 0.33, 'rmvpe', 128, 0, 0.15, 0.2, 0.8, 0.7, 'mp3', None],
+            clear_btn.click(lambda: [0, 0, 0, 0, 0.5, 3, 0.25, 0.33, 'mangio-crepe', 128, 0, 0.15, 0.2, 0.8, 0.7, 'mp3', None],
                             outputs=[pitch, main_gain, backup_gain, inst_gain, index_rate, filter_radius, rms_mix_rate,
                                      protect, f0_method, crepe_hop_length, pitch_all, reverb_rm_size, reverb_wet,
                                      reverb_dry, reverb_damping, output_format, ai_cover])
